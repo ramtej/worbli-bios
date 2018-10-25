448f4540c59485b2140bb2c589b0469e6516b0ce
Jiri Jetmar <juergen.jetmar@gmail.com>
2018-10-25 09:31:19 +0200

    Update 06-create-reserve-accounts.sh
    Added eosgermany RBP

--- scripts/06-create-reserve-accounts.sh
+++ scripts/06-create-reserve-accounts.sh
@@ -7,3 +7,5 @@ worblim system newaccount eosio --transfer eosnationftw EOS5mjZzBi2sp9eWCPvoRAY7
 worblim system newaccount eosio --transfer gravitypool1 EOS6HDHDrDWuWrCyrsLNgch4smwsXNrqM3cz3tz5wvvSsdTepvxHR --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
 
 worblim system newaccount eosio --transfer chainrift EOS7RQ6fJTDzZTSoL6tmzXwfs9FLZTUPWDe6pNAhqqWHvcAxG7qdN --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer eosgermany EOS6TR2SD6XWVNLtutyjF8Zjr5YzNhnq2Ri2EN2S6H5dasVBrR9RE --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
